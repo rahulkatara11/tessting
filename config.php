81927cf2af421b3a4d1f9a01a6cd4de0a81f523a
Rahul <rahulkatara11@gmail.com>
2017-05-22 18:01:22 +0530

    First commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+	define("CLIENT_ID", "3MVG9YDQS5WtC11otqi0Lfbo5kADuctXsOZAUbDUkFAcixp9pxTaBbFoBltSNWsjsB5vzdMIok5bRXraD1EF5");
+
+	define("CLIENT_SECRET", "7310181645345871893");
+
+	define("REDIRECT_URI", "https://localhost/testing/new_test/oauth_callback.php");
+
+	define("LOGIN_URI", "https://login.salesforce.com");
+
+	?>
---
+++ oauth.php
@@ -0,0 +1,11 @@
+<?php
+
+	define("CLIENT_ID", "3MVG9YDQS5WtC11otqi0Lfbo5kADuctXsOZAUbDUkFAcixp9pxTaBbFoBltSNWsjsB5vzdMIok5bRXraD1EF5");
+
+	define("CLIENT_SECRET", "7310181645345871893");
+
+	define("REDIRECT_URI", "https://gentle-waters-89571.herokuapp.com/oauth_callback.php");
+
+	define("LOGIN_URI", "https://login.salesforce.com");
+
+	?>
